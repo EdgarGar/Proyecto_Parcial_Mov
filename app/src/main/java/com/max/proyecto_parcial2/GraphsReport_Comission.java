8803581f98104f826e313c8e031ebff0e27471d7
arthur-alvesaf <arthur.alvesaf@gmail.com>
2017-05-09 16:01:10 -0500

    Graphs and other client stuff

---
+++ app/src/main/java/com/max/proyecto_parcial2/GraphsReport_Comission.java
@@ -0,0 +1,13 @@
+package com.max.proyecto_parcial2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphsReport_Comission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs_report__comission);
+    }
+}
---
+++ app/src/main/java/com/max/proyecto_parcial2/ProductHistoryItem.java
@@ -0,0 +1,8 @@
+package com.max.proyecto_parcial2;
+
+/**
+ * Created by artalves07 on 9/05/17.
+ */
+
+public class ProductHistoryItem {
+}
